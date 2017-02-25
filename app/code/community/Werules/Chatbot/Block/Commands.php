9ec0f9bce59eaebe8382acc64ad28bc41576553d
blopa <montenegro.pablo@outlook.com>
2017-02-24 23:27:25 -0300

    better way to configure commands

---
+++ app/code/community/Werules/Chatbot/Block/Commands.php
@@ -0,0 +1,43 @@
+<?php
+class Werules_Chatbot_Block_Commands extends Mage_Adminhtml_Block_System_Config_Form_Field_Array_Abstract
+{
+	protected $_itemRenderer;
+
+	public function _prepareToRender()
+	{
+		$this->addColumn('command_id', array(
+			'label' => Mage::helper('core')->__('Command'),
+			'renderer' => $this->_getRenderer()
+		));
+		$this->addColumn('command_code', array(
+			'label' => Mage::helper('core')->__('Command Code'),
+			'style' => 'width: 150px'
+		));
+		$this->addColumn('command_alias_list', array(
+			'label' => Mage::helper('core')->__('Alias'),
+			'style' => 'width: 250px'
+		));
+
+		$this->_addAfter = false;
+		$this->_addButtonLabel = Mage::helper('core')->__('Add');
+	}
+
+	protected function _getRenderer()
+	{
+		if (!$this->_itemRenderer) {
+			$this->_itemRenderer = $this->getLayout()->createBlock(
+				'werules_chatbot/commandsselect', '',
+				array('is_render_to_js_template' => true)
+			)->setExtraParams("style='width: auto;'");
+		}
+		return $this->_itemRenderer;
+	}
+
+	protected function _prepareArrayRow(Varien_Object $row)
+	{
+		$row->setData(
+			'option_extra_attr_' . $this->_getRenderer()->calcOptionHash($row->getData('command_id')),
+			'selected="selected"'
+		);
+	}
+}
---
+++ app/code/community/Werules/Chatbot/Block/CommandsSelect.php
@@ -0,0 +1,19 @@
+<?php
+class Werules_Chatbot_Block_CommandsSelect extends Mage_Core_Block_Html_Select
+{
+	public function _toHtml()
+	{
+		$options = Mage::getSingleton('chatbot/options')->toOptionArray();
+		foreach ($options as $option)
+		{
+			$this->addOption($option['value'], $option['label']);
+		}
+
+		return parent::_toHtml();
+	}
+
+	public function setInputName($value)
+	{
+		return $this->setName($value);
+	}
+}
