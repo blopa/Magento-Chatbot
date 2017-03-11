141ae454322c8de927322fb55df164f562134cca
blopa <montenegro.pablo@outlook.com>
2017-03-11 13:07:06 -0300

    Add option to match case

--- app/code/community/Werules/Chatbot/Block/Replies.php
+++ app/code/community/Werules/Chatbot/Block/Replies.php
@@ -14,14 +14,39 @@ class Werules_Chatbot_Block_Replies extends Mage_Adminhtml_Block_System_Config_F
 			'style' => 'width: 250px'
 		));
 		$this->addColumn('similarity', array(
-			'label' => Mage::helper('core')->__('Similarity'),
+			'label' => Mage::helper('core')->__('Similarity (%)'),
 			'style' => 'width: 50px',
 			//'type' => 'number',
 			//'maxlength' => '3',
 			'class' => 'input-number validate-number validate-number-range number-range-1-100'
 		));
+		$this->addColumn('match_case', array(
+			'label' => Mage::helper('core')->__('Match Case'),
+			'renderer' => $this->_getRenderer()
+		));
 
 		$this->_addAfter = false;
 		$this->_addButtonLabel = Mage::helper('core')->__('Add');
+	}
+
+	protected function _getRenderer()
+	{
+		if (!$this->_itemRenderer)
+		{
+			$this->_itemRenderer = $this->getLayout()->createBlock(
+				'werules_chatbot/enable',
+				'',
+				array('is_render_to_js_template' => true)
+			)->setExtraParams("style='width: auto;'");
+		}
+		return $this->_itemRenderer;
+	}
+
+	protected function _prepareArrayRow(Varien_Object $row)
+	{
+		$row->setData(
+			'option_extra_attr_' . $this->_getRenderer()->calcOptionHash($row->getData('match_case')),
+			'selected="selected"'
+		);
 	}
 }
