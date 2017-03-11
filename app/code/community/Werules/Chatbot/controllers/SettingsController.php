5fd72c08c4665c6949dd272be2b093e9066d531b
blopa <montenegro.pablo@outlook.com>
2017-03-10 23:31:59 -0300

    checking if customer is already logged

--- app/code/community/Werules/Chatbot/controllers/SettingsController.php
+++ app/code/community/Werules/Chatbot/controllers/SettingsController.php
@@ -64,86 +64,92 @@ class Werules_Chatbot_SettingsController extends Mage_Core_Controller_Front_Acti
 	{
 		$success = false;
 		$error = false;
+		$logged = true;
 		$data = array();
 		$magehelper = Mage::helper('core');
-		$customerid = Mage::getSingleton('customer/session')->getCustomer()->getId();
-		$chatdata = Mage::getModel('chatbot/chatdata')->load($customerid, 'customer_id');
-		if ($chatdata->getCustomerId()) // check if customer already is on chatdata model
+		$chatdataHash = Mage::getModel('chatbot/chatdata')->load($hash, 'hash_key');
+		if ($chatdataHash->getIsLogged() == "0")
 		{
-			try
+			$logged = false;
+			$customerid = Mage::getSingleton('customer/session')->getCustomer()->getId();
+			$chatdata = Mage::getModel('chatbot/chatdata')->load($customerid, 'customer_id');
+			if ($chatdata->getCustomerId()) // check if customer already is on chatdata model
 			{
-				while ($chatdata->getCustomerId()) // gather all data from all chatdata models
+				try
 				{
-					if ($chatdata->getTelegramChatId() && $chatdata->getFacebookChatId() && $chatdata->getWhatsappChatId() && $chatdata->getWechatChatId())
-						break;
-					if ($chatdata->getTelegramChatId()) {
-						$data["telegram_chat_id"] = $chatdata->getTelegramChatId();
-						$data["telegram_message_id"] = $chatdata->getTelegramMessageId();
-						$data["telegram_conv_state"] = $chatdata->getTelegramConvState();
-						$data["telegram_support_reply_chat_id"] = $chatdata->getTelegramSupportReplyChatId();
+					while ($chatdata->getCustomerId()) // gather all data from all chatdata models
+					{
+						if ($chatdata->getTelegramChatId() && $chatdata->getFacebookChatId() && $chatdata->getWhatsappChatId() && $chatdata->getWechatChatId())
+							break;
+						if ($chatdata->getTelegramChatId()) {
+							$data["telegram_chat_id"] = $chatdata->getTelegramChatId();
+							$data["telegram_message_id"] = $chatdata->getTelegramMessageId();
+							$data["telegram_conv_state"] = $chatdata->getTelegramConvState();
+							$data["telegram_support_reply_chat_id"] = $chatdata->getTelegramSupportReplyChatId();
+						}
+						if ($chatdata->getFacebookChatId()) {
+							$data["facebook_chat_id"] = $chatdata->getFacebookChatId();
+							$data["facebook_message_id"] = $chatdata->getFacebookMessageId();
+							$data["facebook_conv_state"] = $chatdata->getFacebookConvState();
+							$data["facebook_support_reply_chat_id"] = $chatdata->getFacebookSupportReplyChatId();
+						}
+						if ($chatdata->getWhatsappChatId()) {
+							$data["whatsapp_chat_id"] = $chatdata->getWhatsappChatId();
+							$data["whatsapp_message_id"] = $chatdata->getWhatsappMessageId();
+							$data["whatsapp_conv_state"] = $chatdata->getWhatsappConvState();
+							$data["whatsapp_support_reply_chat_id"] = $chatdata->getWhatsappSupportReplyChatId();
+						}
+						if ($chatdata->getWechatChatId()) {
+							$data["wechat_chat_id"] = $chatdata->getWechatChatId();
+							$data["wechat_message_id"] = $chatdata->getWechatMessageId();
+							$data["wechat_conv_state"] = $chatdata->getWechatpConvState();
+							$data["wechat_support_reply_chat_id"] = $chatdata->getWechatSupportReplyChatId();
+						}
+						if (!isset($data["created_at"]))
+							$data["created_at"] = $chatdata->getCreatedAt();
+						$chatdata->delete();
+						$chatdata = Mage::getModel('chatbot/chatdata')->load($customerid, 'customer_id'); // reload
 					}
-					if ($chatdata->getFacebookChatId()) {
-						$data["facebook_chat_id"] = $chatdata->getFacebookChatId();
-						$data["facebook_message_id"] = $chatdata->getFacebookMessageId();
-						$data["facebook_conv_state"] = $chatdata->getFacebookConvState();
-						$data["facebook_support_reply_chat_id"] = $chatdata->getFacebookSupportReplyChatId();
-					}
-					if ($chatdata->getWhatsappChatId()) {
-						$data["whatsapp_chat_id"] = $chatdata->getWhatsappChatId();
-						$data["whatsapp_message_id"] = $chatdata->getWhatsappMessageId();
-						$data["whatsapp_conv_state"] = $chatdata->getWhatsappConvState();
-						$data["whatsapp_support_reply_chat_id"] = $chatdata->getWhatsappSupportReplyChatId();
-					}
-					if ($chatdata->getWechatChatId()) {
-						$data["wechat_chat_id"] = $chatdata->getWechatChatId();
-						$data["wechat_message_id"] = $chatdata->getWechatMessageId();
-						$data["wechat_conv_state"] = $chatdata->getWechatpConvState();
-						$data["wechat_support_reply_chat_id"] = $chatdata->getWechatSupportReplyChatId();
-					}
-					if (!isset($data["created_at"]))
-						$data["created_at"] = $chatdata->getCreatedAt();
-					$chatdata->delete();
-					$chatdata = Mage::getModel('chatbot/chatdata')->load($customerid, 'customer_id'); // reload
-				}
-				if ($data) // if any found, prepare to merge
-				{
-					$data["updated_at"] = date('Y-m-d H:i:s');
-					$data["is_logged"] = "1";
-					$data["customer_id"] = $customerid;
+					if (!empty($data)) // if any found, prepare to merge
+					{
+						$data["updated_at"] = date('Y-m-d H:i:s');
+						$data["is_logged"] = "1";
+						$data["customer_id"] = $customerid;
 
+						$chatdata = Mage::getModel('chatbot/chatdata')->load($hash, 'hash_key');
+						if (!$chatdata->getHashKey())
+							$data["hash_key"] = $hash;
+
+						$chatdata->addData($data);
+						$chatdata->save();
+						$success = true;
+					}
+				}
+				catch (Exception $e)
+				{
+					$error = true;
+				}
+			}
+			else // if is the first time for this customer, just save it
+			{
+				try
+				{
 					$chatdata = Mage::getModel('chatbot/chatdata')->load($hash, 'hash_key');
-					if (!$chatdata->getHashKey())
-						$data["hash_key"] = $hash;
-
-					$chatdata->addData($data);
-					$chatdata->save();
-					$success = true;
+					if ($chatdata->getHashKey()) {
+						$data = array(
+							"customer_id" => $customerid,
+							"is_logged" => "1",
+							"updated_at" => date('Y-m-d H:i:s')
+						);
+						$chatdata->addData($data);
+						$chatdata->save();
+						$success = true;
+					}
 				}
-			}
-			catch (Exception $e)
-			{
-				$error = true;
-			}
-		}
-		else // if is the first time for this customer, just save it
-		{
-			try
-			{
-				$chatdata = Mage::getModel('chatbot/chatdata')->load($hash, 'hash_key');
-				if ($chatdata->getHashKey()) {
-					$data = array(
-						"customer_id" => $customerid,
-						"is_logged" => "1",
-						"updated_at" => date('Y-m-d H:i:s')
-					);
-					$chatdata->addData($data);
-					$chatdata->save();
-					$success = true;
+				catch (Exception $e)
+				{
+					$error = true;
 				}
-			}
-			catch (Exception $e)
-			{
-				$error = true;
 			}
 		}
 		// messages
@@ -151,5 +157,7 @@ class Werules_Chatbot_SettingsController extends Mage_Core_Controller_Front_Acti
 			Mage::getSingleton('customer/session')->addSuccess($magehelper->__("Your account is now attached with our chatbot."));
 		if ($error)
 			Mage::getSingleton('customer/session')->addError($magehelper->__("Something went wrong, please try again."));
+		if ($logged)
+			Mage::getSingleton('customer/session')->addNotice($magehelper->__("You're already logged."));
 	}
 }
