4385e2fa534bd9c5a81ed0b01b5b728f6210aa9a
blopa <montenegro.pablo@outlook.com>
2017-03-22 19:04:54 -0300

    add option to select a command as a default reply

--- Magento-Root/app/code/community/Werules/Chatbot/Block/Replies.php
+++ Magento-Root/app/code/community/Werules/Chatbot/Block/Replies.php
@@ -3,7 +3,9 @@ class Werules_Chatbot_Block_Replies extends Mage_Adminhtml_Block_System_Config_F
 {
 	protected $_itemRendererEnableCase;
 	protected $_itemRendererEnableProcessing;
+	protected $_itemRendererMatchMode;
 	protected $_itemRendererReplyMode;
+	protected $_itemRendererCommandCode;
 
 	public function _prepareToRender()
 	{
@@ -21,7 +23,7 @@ class Werules_Chatbot_Block_Replies extends Mage_Adminhtml_Block_System_Config_F
 		));
 		$this->addColumn('reply_mode', array(
 			'label' => Mage::helper('core')->__('Reply Mode'),
-			'renderer' => $this->_getRendererReplyMode()
+			'renderer' => $this->_getRendererMatchMode()
 		));
 		$this->addColumn('similarity', array(
 			'label' => Mage::helper('core')->__('Similarity (%)'),
@@ -31,8 +33,12 @@ class Werules_Chatbot_Block_Replies extends Mage_Adminhtml_Block_System_Config_F
 			'class' => 'input-number validate-number validate-number-range number-range-1-100'
 		));
 		$this->addColumn('reply_phrase', array(
-			'label' => Mage::helper('core')->__('Reply'),
+			'label' => Mage::helper('core')->__('Reply Text'),
 			'style' => 'width: 250px'
+		));
+		$this->addColumn('command_id', array(
+			'label' => Mage::helper('core')->__('Command'),
+			'renderer' => $this->_getRendererCommandCode()
 		));
 
 		$this->_addAfter = false;
@@ -67,18 +73,31 @@ class Werules_Chatbot_Block_Replies extends Mage_Adminhtml_Block_System_Config_F
 		return $this->_itemRendererEnableProcessing;
 	}
 
-	protected function _getRendererReplyMode()
+	protected function _getRendererCommandCode()
 	{
-		if (!$this->_itemRendererReplyMode)
+		if (!$this->_itemRendererCommandCode)
 		{
-			$this->_itemRendererReplyMode = $this->getLayout()->createBlock(
+			$this->_itemRendererCommandCode = $this->getLayout()->createBlock(
+				'werules_chatbot/commandsSelect',
+				'',
+				array('is_render_to_js_template' => true)
+			)->setExtraParams("style='width: 100%;'");
+		}
+		return $this->_itemRendererCommandCode;
+	}
+
+	protected function _getRendererMatchMode()
+	{
+		if (!$this->_itemRendererMatchMode)
+		{
+			$this->_itemRendererMatchMode = $this->getLayout()->createBlock(
 				//'werules_chatbot/enable',
 				'werules_chatbot/replyMode',
 				'',
 				array('is_render_to_js_template' => true)
 			)->setExtraParams("style='width: auto;'");
 		}
-		return $this->_itemRendererReplyMode;
+		return $this->_itemRendererMatchMode;
 	}
 
 	protected function _prepareArrayRow(Varien_Object $row)
@@ -92,7 +111,11 @@ class Werules_Chatbot_Block_Replies extends Mage_Adminhtml_Block_System_Config_F
 			'selected="selected"'
 		);
 		$row->setData(
-			'option_extra_attr_' . $this->_getRendererReplyMode()->calcOptionHash($row->getData('reply_mode')),
+			'option_extra_attr_' . $this->_getRendererMatchMode()->calcOptionHash($row->getData('reply_mode')),
+			'selected="selected"'
+		);
+		$row->setData(
+			'option_extra_attr_' . $this->_getRendererCommandCode()->calcOptionHash($row->getData('command_id')),
 			'selected="selected"'
 		);
 	}
