2665be66e7acaab085313bca6719c600b931e93a
blopa <montenegro.pablo@outlook.com>
2017-04-09 20:33:37 -0300

    better label for regex

--- Magento-Root/app/code/community/Werules/Chatbot/Model/MatchMode.php
+++ Magento-Root/app/code/community/Werules/Chatbot/Model/MatchMode.php
@@ -13,7 +13,7 @@ class Werules_Chatbot_Model_MatchMode
 			array('value'=>1, 'label'=>'Starts With'),
 			array('value'=>2, 'label'=>'Ends With'),
 			array('value'=>3, 'label'=>'Contains'),
-			array('value'=>4, 'label'=>'Match Regular Expression'),
+			array('value'=>4, 'label'=>'Match RegEx'),
 			array('value'=>5, 'label'=>'Equals to'),
 			array('value'=>6, 'label'=>'wit.ai')
 		);
