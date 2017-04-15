b5043f5f15a1a27edf2b797c81810c3f43e2860e
blopa <montenegro.pablo@outlook.com>
2017-04-15 18:04:49 -0300

    create getAudioResponse

--- Magento1/app/code/community/Werules/Chatbot/Model/Api/witAI/witAI.php
+++ Magento1/app/code/community/Werules/Chatbot/Model/Api/witAI/witAI.php
@@ -3,7 +3,7 @@
 	{
 		protected $_token;
 		protected $_version = '20170415';
-		protected $_data;
+		//protected $_data;
 
 		/// Class constructor
 		public function __construct($token) {
@@ -12,28 +12,40 @@
 
 		function getTextResponse($query)
 		{
+			$options = array(
+				'http' => array(
+					'method' => 'GET',
+					'header' => "Authorization: Bearer " . $this->_token . "\r\n"
+				)
+			);
 			$content = "&q=" . urlencode($query);
-			return $this->getWitAIResponse("message", $content);
+			return $this->getWitAIResponse("message", $content, $options);
 		}
 
-		function getWitAIResponse($endpoint, $content)
+		function getAudioResponse($audioFile)
 		{
-			if (!isset($this->_data))
-			{
-				$accessToken = $this->_token;
-				$options = array(
-					'http' => array(
-						'method' => 'GET',
-						'header' => "Authorization: Bearer " . $accessToken . "\r\n"
-					)
-				);
-				$context = stream_context_create($options);
-				$url = 'https://api.wit.ai/' . $endpoint . '?v=' . $this->_version . $content;
-				$result = file_get_contents($url, false, $context);
-				$result = json_decode($result);
-				$this->_data = $result;
-			}
-			return $this->_data;
+			$options = array(
+				'http' => array(
+					'method' => 'POST',
+					'header' => "Authorization: Bearer " . $this->_token . "\n" .
+							"Content-Type: audio/wav" . "\r\n",
+					'content' => file_get_contents($audioFile)
+				)
+			);
+			return $this->getWitAIResponse("speech", "", $options);
+		}
+
+		function getWitAIResponse($endPoint, $content, $options)
+		{
+			$context = stream_context_create($options);
+			$url = 'https://api.wit.ai/' . $endPoint . '?v=' . $this->_version . $content;
+			$result = file_get_contents($url, false, $context);
+			$result = json_decode($result);
+
+			if ($result)
+				return $result;
+
+			return null;
 		}
 	}
 ?>
