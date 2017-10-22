810fc4f3e559a28f92041de335bed5f5c0346c65
blopa <montenegro.pablo@outlook.com>
2017-10-22 13:43:01 -0200

    creating empty page to output json

--- Magento2/app/code/Werules/Chatbot/Controller/Index/Index.php
+++ Magento2/app/code/Werules/Chatbot/Controller/Index/Index.php
@@ -1,22 +1,20 @@
 <?php
-
 namespace Werules\Chatbot\Controller\Index;
 
-use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultFactory;
 
-class Index extends \Magento\Framework\App\Action\Action
+class Index extends Action
 {
-	protected $_resultPageFactory;
-
-	public function __construct(Context $context, \Magento\Framework\View\Result\PageFactory $resultPageFactory)
-	{
-		$this->_resultPageFactory = $resultPageFactory;
-		parent::__construct($context);
-	}
-
+	/**
+	 * Dispatch request
+	 *
+	 * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+	 * @throws \Magento\Framework\Exception\NotFoundException
+	 */
 	public function execute()
 	{
-		$resultPage = $this->_resultPageFactory->create();
-		return $resultPage;
+		return $this->resultFactory->create(ResultFactory::TYPE_LAYOUT);
 	}
 }
