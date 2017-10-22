db46b5aa52c4fb1e97ebeee6055924a60b58704d
blopa <montenegro.pablo@outlook.com>
2017-10-22 20:28:49 -0200

    update comment

--- Magento2/app/code/Werules/Chatbot/Cron/Worker.php
+++ Magento2/app/code/Werules/Chatbot/Cron/Worker.php
@@ -11,10 +11,15 @@ class Worker {
 
 	/**
 	 * Method executed when cron runs in server
-	 */
+	*/
 	public function execute() {
 //		if (shell_exec('ps aux | grep ' . __FILE__ . ' | wc  -l') > 1) {
 //			exit('already running...');
+//		}
+//		OR
+//		$f = fopen('lock', 'w') or die ('Cannot create lock file');
+//		if (flock($f, LOCK_EX | LOCK_NB)) {
+//			// yay
 //		}
 		$this->_logger->debug('Running Cron from Worker class');
 		return $this;
