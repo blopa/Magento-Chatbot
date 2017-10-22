2efa22e80098ec8174816a98b88656f1e34f8ade
blopa <montenegro.pablo@outlook.com>
2017-10-22 16:00:14 -0200

    fix problem returning hello world

--- Magento2/app/code/Werules/Chatbot/Controller/Index/Index.php
+++ Magento2/app/code/Werules/Chatbot/Controller/Index/Index.php
@@ -13,6 +13,14 @@ class Index extends Action
 	 * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
 	 * @throws \Magento\Framework\Exception\NotFoundException
 	 */
+//	protected $_resultFactory;
+//
+//	public function __construct(\Magento\Framework\App\Action\Context $context, ResultFactory $resultFactory)
+//	{
+//		$this->_resultFactory = $resultFactory;
+//		parent::__construct($context);
+//	}
+
 	public function execute()
 	{
 		return $this->resultFactory->create(ResultFactory::TYPE_LAYOUT);
