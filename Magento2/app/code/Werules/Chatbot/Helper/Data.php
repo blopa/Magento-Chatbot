87b1faa7cb06b260ede4f0fdd5281d77ace6b0fc
blopa <montenegro.pablo@outlook.com>
2017-11-04 01:29:26 -0200

    add backend configurations

---
+++ Magento2/app/code/Werules/Chatbot/Helper/Data.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Magento Chatbot Integration
+ * Copyright (C) 2017
+ *
+ * This file is part of Werules/Chatbot.
+ *
+ * Werules/Chatbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Werules\Chatbot\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Framework\App\Helper\Context;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+    protected $storeManager;
+    protected $objectManager;
+
+    const XML_PATH_CHATBOT = 'chatbot/';
+
+    public function __construct(Context $context,
+        ObjectManagerInterface $objectManager,
+        StoreManagerInterface $storeManager
+    )
+    {
+        $this->objectManager = $objectManager;
+        $this->storeManager  = $storeManager;
+        parent::__construct($context);
+    }
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+
+    public function getGeneralConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_CHATBOT . $code, $storeId);
+    }
+}
