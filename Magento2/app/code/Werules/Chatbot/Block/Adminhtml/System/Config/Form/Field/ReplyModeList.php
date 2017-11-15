18968bec76ede5f516528b766186f3571b95981c
blopa <montenegro.pablo@outlook.com>
2017-11-15 14:24:29 -0200

    add NLP to array configutarion

---
+++ Magento2/app/code/Werules/Chatbot/Block/Adminhtml/System/Config/Form/Field/ReplyModeList.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Magento Chatbot Integration
+ * Copyright (C) 2017
+ *
+ * This file is part of Werules/Chatbot.
+ *
+ * Werules/Chatbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Werules\Chatbot\Block\Adminhtml\System\Config\Form\Field;
+
+class ReplyModeList implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Provide available options as a value/label array
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return array(
+            array('value'=>0, 'label'=>'No Reply'),
+            array('value'=>1, 'label'=>'Text Only'),
+            array('value'=>2, 'label'=>'Command Only'),
+            array('value'=>3, 'label'=>'Text and Command')
+        );
+    }
+}
