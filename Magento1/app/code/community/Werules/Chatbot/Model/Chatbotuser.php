21cdcedd567b05f14d2b3061f205d0195c0e0282
blopa <montenegro.pablo@outlook.com>
2017-12-10 01:39:39 -0200

    add magento1 first draft

---
+++ Magento1/app/code/community/Werules/Chatbot/Model/Chatbotuser.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Magento Chatbot Integration
+ * Copyright (C) 2017
+ *
+ * This file is part of Werules/Chatbot.
+ *
+ * Werules/Chatbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class Werules_Chatbot_Model_Chatbotuser extends Mage_Core_Model_Abstract
+{
+    protected function _construct(){
+
+       $this->_init("chatbot/chatbotuser");
+
+    }
+
+}
