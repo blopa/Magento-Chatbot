8cad9ecf24520b1134cb86f8feed91c6e58dba91
blopa <montenegro.pablo@outlook.com>
2017-12-24 10:55:39 -0200

    add default.xml and chatbox block

---
+++ Magento2/app/code/Werules/Chatbot/Block/Chatbox/Messenger.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Magento Chatbot Integration
+ * Copyright (C) 2017  
+ * 
+ * This file is part of Werules/Chatbot.
+ * 
+ * Werules/Chatbot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Werules\Chatbot\Block\Chatbox;
+
+class Messenger extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = array()
+    )
+    {
+        parent::__construct($context, $data);
+    }
+}
