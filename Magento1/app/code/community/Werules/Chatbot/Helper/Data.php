3b0b85f2a26753c23b87c4aa288fb58773701826
blopa <montenegro.pablo@outlook.com>
2018-01-01 13:57:48 -0200

    add new functions

--- Magento1/app/code/community/Werules/Chatbot/Helper/Data.php
+++ Magento1/app/code/community/Werules/Chatbot/Helper/Data.php
@@ -106,9 +106,252 @@ class Werules_Chatbot_Helper_Data extends Mage_Core_Helper_Abstract
         return $this->_messageQueueMode;
     }
 
-    public function processIncomingMessage($message) // TODO_NOW
+    private function setConfigPrefix($chatbotType)
     {
-        return array();
+        if (!isset($this->_configPrefix))
+        {
+            if ($chatbotType == $this->_define->MESSENGER_INT)
+                $this->_configPrefix = 'werules_chatbot_messenger';
+        }
+    }
+
+    private function getChatbotAPIModelBySenderId($senderId)
+    {
+        if (isset($this->_chatbotAPIModel))
+            return $this->_chatbotAPIModel;
+
+        // should never get here
+        // because it's already set
+        $chatbotAPI = $this->getChatbotAPIBySenderId($senderId);
+        $this->setChatbotAPIModel($chatbotAPI);
+
+        return $chatbotAPI;
+    }
+
+    private function setChatbotAPIModel($chatbotAPI)
+    {
+        $this->_chatbotAPIModel = $chatbotAPI;
+    }
+
+    private function getChatbotAPIBySenderId($senderId)
+    {
+        $chatbotAPI = $this->_chatbotAPIFactory->create();
+        $chatbotAPI->load($senderId, 'chat_id'); // TODO
+
+        return $chatbotAPI;
+    }
+
+    public function createChatbotAPI($chatbotAPI, $message)
+    {
+        $chatbotAPI->setEnabled($this->_define->ENABLED);
+        $chatbotAPI->setChatbotType($message->getChatbotType());
+        $chatbotAPI->setChatId($message->getSenderId());
+        $chatbotAPI->setConversationState($this->_define->CONVERSATION_STARTED);
+        $chatbotAPI->setFallbackQty(0);
+        $chatbotAPI->setLastCommandDetails($this->_define->LAST_COMMAND_DETAILS_DEFAULT); // TODO
+        $hash = $this->generateRandomHashKey();
+        $chatbotAPI->setHashKey($hash);
+        $datetime = date('Y-m-d H:i:s');
+        $chatbotAPI->setCreatedAt($datetime);
+        $chatbotAPI->setUpdatedAt($datetime);
+        $chatbotAPI->save();
+
+        return $chatbotAPI;
+    }
+
+    private function getTextMessageArray($text)
+    {
+        $result = array();
+        $responseMessage = array(
+            'content_type' => $this->_define->CONTENT_TEXT,
+            'content' => $text,
+            'current_command_details' => json_encode(array())
+        );
+        array_push($result, $responseMessage);
+        return $result;
+    }
+
+    private function getErrorMessage()
+    {
+        $text = __("Something went wrong, please try again.");
+        return $this->getTextMessageArray($text);
+    }
+
+    private function getDisabledMessage($message)
+    {
+        $outgoingMessages = array();
+        $text = $this->getConfigValue($this->_configPrefix . '/general/disabled_message');
+
+        if ($text != '')
+            $contentObj = $this->getTextMessageArray($text);
+        else
+            $contentObj = $this->getErrorMessage();
+
+        $outgoingMessage = $this->createOutgoingMessage($message, reset($contentObj)); // TODO reset -> gets first item of array
+        if ($outgoingMessage)
+            array_push($outgoingMessages, $outgoingMessage);
+
+        return $outgoingMessages;
+    }
+
+    private function getDisabledByCustomerMessage($message)
+    {
+        $outgoingMessages = array();
+        $text = __("To chat with me, please enable Messenger on your account chatbot settings.");
+        $contentObj = $this->getTextMessageArray($text);
+        $outgoingMessage = $this->createOutgoingMessage($message, reset($contentObj)); // TODO reset -> gets first item of array
+        if ($outgoingMessage)
+            array_push($outgoingMessages, $outgoingMessage);
+
+        return $outgoingMessages;
+    }
+
+    public function processIncomingMessage($message)
+    {
+        $messageQueueMode = $this->getQueueMessageMode();
+        if (($messageQueueMode == $this->_define->QUEUE_NONE) && ($message->getStatus() != $this->_define->PROCESSED))
+            $message->updateIncomingMessageStatus($this->_define->PROCESSED);
+        else if ($message->getStatus() != $this->_define->PROCESSING)
+            $message->updateIncomingMessageStatus($this->_define->PROCESSING);
+
+        $this->setConfigPrefix($message->getChatbotType());
+        $chatbotAPI = $this->getChatbotAPIModelBySenderId($message->getSenderId());
+        $result = array();
+
+        if (!($chatbotAPI->getChatbotapiId()))
+        {
+            $chatbotAPI = $this->createChatbotAPI($chatbotAPI, $message);
+            $welcomeMessage = $this->getWelcomeMessage($message);
+            if ($welcomeMessage)
+            {
+                if ($message->getStatus() != $this->_define->PROCESSED)
+                    $message->updateIncomingMessageStatus($this->_define->PROCESSED);
+
+                array_push($result, $welcomeMessage);
+                return $result;
+            }
+        }
+
+        $enabled = $this->getConfigValue($this->_configPrefix . '/general/enable');
+        if ($enabled == $this->_define->DISABLED)
+            $outgoingMessages = $this->getDisabledMessage($message);
+        else if ($chatbotAPI->getEnabled() == $this->_define->DISABLED)
+            $outgoingMessages = $this->getDisabledByCustomerMessage($message);
+        else
+        {
+            // all good, let's process the request
+            $this->setChatbotAPIModel($chatbotAPI);
+            $outgoingMessages = $this->prepareOutgoingMessage($message);
+        }
+
+        if ($outgoingMessages)
+        {
+            foreach ($outgoingMessages as $outgoingMessage)
+            {
+                array_push($result, $outgoingMessage);
+            }
+        }
+
+        if (($result) && ($message->getStatus() != $this->_define->PROCESSED))
+            $message->updateIncomingMessageStatus($this->_define->PROCESSED);
+
+        return $result;
+    }
+
+
+
+    private function prepareOutgoingMessage($message)
+    {
+        $responseContents = $this->processMessageRequest($message);
+        $outgoingMessages = array();
+
+        if ($responseContents)
+        {
+            foreach ($responseContents as $content)
+            {
+                // first guarantee outgoing message is saved
+                $outgoingMessage = $this->createOutgoingMessage($message, $content);
+                array_push($outgoingMessages, $outgoingMessage);
+            }
+        }
+
+        return $outgoingMessages;
+    }
+
+    private function getWelcomeMessage($message)
+    {
+        $outgoingMessage = array();
+        $text = $this->getConfigValue($this->_configPrefix . '/general/welcome_message');
+        if ($text != '')
+        {
+            $enableMessageOptions = $this->getConfigValue($this->_configPrefix . '/general/enable_message_options');
+            if ($enableMessageOptions == $this->_define->ENABLED)
+            {
+                $quickReplies = array();
+                $welcomeMessageOptions = $this->getWelcomeMessageOptionsData();
+                foreach ($welcomeMessageOptions as $optionId => $messageOption)
+                {
+                    if (count($quickReplies) >= $this->_define->MAX_MESSAGE_ELEMENTS)
+                        break;
+
+                    $quickReply = array(
+                        'content_type' => 'text', // TODO messenger pattern
+                        'title' => $messageOption['option_text'],
+                        'payload' => json_encode(array())
+                    );
+                    array_push($quickReplies, $quickReply);
+                }
+
+                $contentObject = new stdClass();
+                $contentObject->message = $text;
+                $contentObject->quick_replies = $quickReplies;
+                $content = json_encode($contentObject);
+                $contentType = $this->_define->QUICK_REPLY;
+            }
+            else
+            {
+                $contentType = $this->_define->CONTENT_TEXT;
+                $content = $text;
+            }
+            $responseMessage = array(
+                'content_type' => $contentType,
+                'content' => $content,
+                'current_command_details' => json_encode(array())
+            );
+            $outgoingMessage = $this->createOutgoingMessage($message, $responseMessage);
+        }
+
+        return $outgoingMessage;
+    }
+
+    public function createOutgoingMessage($message, $content)
+    {
+        $outgoingMessage = $this->_messageModelFactory;
+        $outgoingMessage->setSenderId($message->getSenderId());
+        $outgoingMessage->setContent($content['content']);
+        $outgoingMessage->setContentType($content['content_type']); // TODO
+        $outgoingMessage->setCurrentCommandDetails($content['current_command_details']); // TODO
+        $outgoingMessage->setStatus($this->_define->PROCESSING);
+        $outgoingMessage->setDirection($this->_define->OUTGOING);
+        $outgoingMessage->setChatMessageId($message->getChatMessageId());
+        $outgoingMessage->setChatbotType($message->getChatbotType());
+        $outgoingMessage->setSentAt(time());
+        $datetime = date('Y-m-d H:i:s');
+        $outgoingMessage->setCreatedAt($datetime);
+        $outgoingMessage->setUpdatedAt($datetime);
+        $outgoingMessage->save();
+
+        return $outgoingMessage;
+    }
+
+    private function getWelcomeMessageOptionsData()
+    {
+        $welcomeMessageOptions = array();
+        $serializedWelcomeMessageOptions = $this->getConfigValue($this->_configPrefix . '/general/message_options');
+        if ($serializedWelcomeMessageOptions)
+            $welcomeMessageOptions = $this->_serializer->unserialize($serializedWelcomeMessageOptions);
+
+        return $welcomeMessageOptions;
     }
 
     public function processOutgoingMessage($outgoingMessage) // TODO_NOW
